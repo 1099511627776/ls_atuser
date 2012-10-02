16669f6685fc18b70ac9d73f5230d230df2ed4f8
gigabyte <1099511627776@mail.ru>
2012-10-02 14:08:01 +0300

    Initial commit

---
+++ atuser/PluginAtuser.class.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Запрещаем напрямую через браузер обращение к этому файлу.
+ */
+if (!class_exists('Plugin')) {
+    die('Hacking attemp!');
+}
+
+class PluginAtuser extends Plugin {
+
+	protected $aInherits=array(
+		'module'=>array('ModuleText'),
+	);
+
+    // Активация плагина
+    public function Activate() {
+        return true;
+    }
+
+    // Деактивация плагина
+    public function Deactivate(){       
+    	return true;
+    }
+    // Инициализация плагина
+    public function Init() {
+    }
+}
+?>
