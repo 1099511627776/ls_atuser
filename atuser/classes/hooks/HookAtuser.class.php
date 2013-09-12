941bdcf600954b1c18923e24543df2c9d885c7de
gigabyte <1099511627776@mail.ru>
2013-09-12 16:55:50 +0300

    + #tag -> /tag/...../ convertor

--- atuser/classes/hooks/HookAtuser.class.php
+++ atuser/classes/hooks/HookAtuser.class.php
@@ -4,67 +4,71 @@ class PluginAtuser_HookAtuser extends Hook {
 
     /*
      * Регистрация событий на хуки
-	*/
+    */
+    protected function processHashTag($match){
+        $word = $match[1];
+        return '<a class="inline-tag" href="'.Router::GetPath('tag').$word.'">'.$word.'</a>';
+    }
 
-	protected function makeCorrection($sText,$template,$aAssign=array()){
-		$match = array();
-		preg_match_all('/@\w+/u',$sText,$match);
-		$repls = array();
-		foreach($match as $vals){
-			if(count($vals) != 0) {
-				foreach ($vals as $val){
-					$login = substr(trim($val),1);
-					if($oUser = $this->User_GetUserByLogin($login)) {
-						$repls[] = array('repl'=>$val,'ref'=>$oUser->getUserWebPath(),'login'=>$oUser->getLogin());
-					}
-				}
-			}
-		}
-		$sRes = $sText;
-		foreach($repls as $repl) {
-			$sRes = str_replace($repl['repl'],'<a href="'.$repl['ref'].'" class="ls-user">'.$repl['login'].'</a>',$sRes);
-		}
-		preg_match_all("/<a.*?class=[\"']ls-user[\"']\s*?>(.*?)<\/a>/u",$sText,$match);
-		$matches = array_unique($match[1]);
-		$sNotifyTitle = $this->Lang_Get('plugin.atuser.notify_title');
-		foreach($matches as $val){				
-			$login = trim($val);
-			if($oUser = $this->User_GetUserByLogin($login)) {
-				if($template != ''){
-					$params=array('oUser'=>$oUser);
-					$params = array_merge($params,$aAssign);
-					$this->Notify_Send($oUser,$template,$sNotifyTitle,$params,'atuser');							
-				}
-			}
-		}
-		return $sRes;
-	}
+    protected function makeHashTag($sText){
+        return preg_replace_callback("/#(\S+[^\s])/misu",array($this,"processHashTag"),$sText);
+    }
+
+    protected function makeAtUser($sText,$template,$aAssign=array()){
+        $match = array();
+        preg_match_all('/@\w+/u',$sText,$match);
+        $repls = array();
+        foreach($match as $vals){
+            if(count($vals) != 0) {
+                foreach ($vals as $val){
+                    $login = substr(trim($val),1);
+                    if($oUser = $this->User_GetUserByLogin($login)) {
+                        $repls[] = array('repl'=>$val,'ref'=>$oUser->getUserWebPath(),'login'=>$oUser->getLogin());
+                    }
+                }
+            }
+        }
+        $sRes = $sText;
+        foreach($repls as $repl) {
+            $sRes = str_replace($repl['repl'],'<a href="'.$repl['ref'].'" class="ls-user">'.$repl['login'].'</a>',$sRes);
+        }
+        preg_match_all("/<a.*?class=[\"']ls-user[\"']\s*?>(.*?)<\/a>/u",$sText,$match);
+        $matches = array_unique($match[1]);
+        $sNotifyTitle = $this->Lang_Get('plugin.atuser.notify_title');
+        foreach($matches as $val){
+            $login = trim($val);
+            if($oUser = $this->User_GetUserByLogin($login)) {
+                if($template != ''){
+                    $params=array('oUser'=>$oUser);
+                    $params = array_merge($params,$aAssign);
+                    $this->Notify_Send($oUser,$template,$sNotifyTitle,$params,'atuser');
+                }
+            }
+        }
+        return $sRes;
+    }
     public function RegisterHook() {
         $this->AddHook('comment_add_before', 'correctComment',__CLASS__);
-        $this->AddHook('comment_item_add_after', 'correctComment',__CLASS__);        
+        $this->AddHook('comment_item_add_after', 'correctComment',__CLASS__);
     }
-	
-	public function correctComment($params){
-		$oComment = $params['oCommentNew'];
-		$oSender = $this->User_GetUserById($oComment->getUserId());
-		$oTopic = $this->Topic_GetTopicById($oComment->getTargetId());
-		/*print_r($oSender);
-		print_r($oTopic);
-		print_r($oComment);*/
+    
+    public function correctComment($params){
+        $oComment = $params['oCommentNew'];
+        $oSender = $this->User_GetUserById($oComment->getUserId());
+        $oTopic = $this->Topic_GetTopicById($oComment->getTargetId());
+        $sRes = $this->makeAtUser($oComment->getText(),
+            'notify.comment_mention.tpl',
+            array('oComment'=>$oComment,'oSender'=>$oSender,'oTopic'=>$oTopic)
+        );
+        $oComment->setText($sRes);
+        $oComment->setTextHash(md5($sRes));
+    }
 
-		$sRes = $this->makeCorrection($oComment->getText(),
-			'notify.comment_mention.tpl',
-			array('oComment'=>$oComment,'oSender'=>$oSender,'oTopic'=>$oTopic)
-		);
-		$oComment->setText($sRes);
-		$oComment->setTextHash(md5($sRes));
-	}
-
-	public function correctTopic($params){
-		$oTopic = $params['oTopic'];
-		$sRes = $this->makeCorrection($oTopic->getText(),'');
-		$oTopic->setText($sRes);
-		$oTopic->setTextHash(md5($sRes));
-	}
+    public function correctTopic($params){
+        $oTopic = $params['oTopic'];
+        $sRes = $this->makeAtUser($oTopic->getText(),'');
+        $oTopic->setText($sRes);
+        $oTopic->setTextHash(md5($sRes));
+    }
 }
 ?>
--- atuser/classes/modules/text/Text.class.php
+++ atuser/classes/modules/text/Text.class.php
@@ -2,32 +2,42 @@
 
 class PluginAtuser_ModuleText extends PluginAtuser_Inherit_ModuleText
 {
-	protected function makeCorrection($sText){
-		$match = array();
-		preg_match_all('/@\w+/u',$sText,$match);
-		$repls = array();
-		foreach($match as $vals){
-			if(count($vals) != 0) {
-				foreach ($vals as $val){
-					$login = substr(trim($val),1);
-					if($oUser = $this->User_GetUserByLogin($login)) {
-						$repls[] = array('repl'=>$val,'ref'=>$oUser->getUserWebPath(),'login'=>$oUser->getLogin());
-					}
-				}
-			}
-		}
-		$sRes = $sText;
-		foreach($repls as $repl) {
-			$sRes = str_replace($repl['repl'],'<a href="'.$repl['ref'].'" class="ls-user">'.$repl['login'].'</a>',$sRes);
-		}
-		return $sRes;
-	}
-	
-	public function Parser($sText) {
-		$sResult = parent::Parser($sText);
-		$sResult = $this->makeCorrection($sResult);
-		return $sResult;
-	}
+    protected function processHashTag($match){
+        $word = $match[1];
+        return '<a class="inline-tag" href="'.Router::GetPath('tag').$word.'">'.$word.'</a>';
+    }
+
+    protected function makeHashTag($sText){
+        return preg_replace_callback("/#(\S+[^\s])/misu",array($this,"processHashTag"),$sText);
+    }
+
+    protected function makeAtUser($sText){
+        $match = array();
+        preg_match_all('/@\w+/u',$sText,$match);
+        $repls = array();
+        foreach($match as $vals){
+            if(count($vals) != 0) {
+                foreach ($vals as $val){
+                    $login = substr(trim($val),1);
+                    if($oUser = $this->User_GetUserByLogin($login)) {
+                        $repls[] = array('repl'=>$val,'ref'=>$oUser->getUserWebPath(),'login'=>$oUser->getLogin());
+                    }
+                }
+            }
+        }
+        $sRes = $sText;
+        foreach($repls as $repl) {
+            $sRes = str_replace($repl['repl'],'<a href="'.$repl['ref'].'" class="ls-user">'.$repl['login'].'</a>',$sRes);
+        }
+        return $sRes;
+    }
+    
+    public function Parser($sText) {
+        $sResult = parent::Parser($sText);
+        $sResult = $this->makeAtUser($sResult);
+        $sResult = $this->makeHashtag($sResult);
+        return $sResult;
+    }
 }
 
 ?>
